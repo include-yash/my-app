28b2d6573a14c7e9f2a27374fdb4a0cf38d12006
Yash <er.yshsingh@gmail.com>
2024-12-22 18:38:51 +0530

    1-project setup

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,17 @@
+export const signup = async (req, res) => {
+    res.json({
+        message: "User created successfully",
+    })
+}
+
+export const login = async (req, res) => {
+    res.json({
+        message: "User created successfully",
+    })
+}
+
+export const logout = async (req, res) => {
+    res.json({
+        message: "User created successfully",
+    })
+}
--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,21 @@
 import express from 'express';
+import authRoutes from './routes/auth.routes.js'
+import dotenv from 'dotenv';
+import {connectMongoDB} from './db/connectMongoDB.js'
+
 
 const app = express();
+const PORT = process.env.PORT || 5000;
 
+dotenv.config();
+
+app.use("/api/auth", authRoutes);
 app.get("/",(req, res)=>{
     res.send("Server is running")
 })
 
-app.listen(8000,()=>{
-    console.log("Server is running on port 8000")
+
+app.listen(PORT,()=>{
+    console.log("Server is running on port 8000");
+    connectMongoDB();
 })
